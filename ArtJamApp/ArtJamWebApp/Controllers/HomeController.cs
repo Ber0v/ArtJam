2c88b1474866f11df73458df16793dd2bfcaec40
Ber0v <AlexBerov123@gmail.com>
2024-10-26 18:23:19 +0300

    creating a site look
    change controller and _Layout

--- ArtJamApp/ArtJamWebApp/Controllers/HomeController.cs
+++ ArtJamApp/ArtJamWebApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using ArtJamWebApp.Data;
 using ArtJamWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ArtJamWebApp.Controllers
@@ -7,15 +9,65 @@ namespace ArtJamWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ArtJamDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ArtJamDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string searchQuery)
         {
-            return View();
+            // Създаваме нов HomeViewModel
+            var viewModel = new HomeViewModel
+            {
+                GroupIds = new List<int>(),
+                GroupNames = new List<string>(),
+                MusicianIds = new List<int>(),
+                MusicianUsernames = new List<string>(),
+                EventIds = new List<int>(),
+                EventNames = new List<string>(),
+                EventDates = new List<DateTime>(),
+                SearchQuery = searchQuery
+            };
+
+            // Попълване на групи
+            var groups = await _context.Groups
+                .Where(g => string.IsNullOrEmpty(searchQuery) || g.GroupName.Contains(searchQuery))
+                .ToListAsync();
+
+            foreach (var group in groups)
+            {
+                viewModel.GroupIds.Add(group.Id);
+                viewModel.GroupNames.Add(group.GroupName);
+            }
+
+            // Попълване на музиканти
+            var musicians = await _context.MusicianProfiles
+                .Where(m => string.IsNullOrEmpty(searchQuery) || m.User.UserName.Contains(searchQuery))
+                .ToListAsync();
+
+            foreach (var musician in musicians)
+            {
+                viewModel.MusicianIds.Add(musician.Id);
+                viewModel.MusicianUsernames.Add(musician.User.UserName);
+            }
+
+            // Попълване на събития
+            var upcomingEvents = await _context.Events
+                .Where(e => e.Date >= DateTime.Now)
+                .OrderBy(e => e.Date)
+                .ToListAsync();
+
+            foreach (var eventItem in upcomingEvents)
+            {
+                viewModel.EventIds.Add(eventItem.Id);
+                viewModel.EventNames.Add(eventItem.Title);
+                viewModel.EventDates.Add(eventItem.Date);
+            }
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ ArtJamApp/ArtJamWebApp/Models/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ArtJamWebApp.Models
+{
+    public class HomeViewModel
+    {
+        // Информация за групите
+        public List<int> GroupIds { get; set; }
+        public List<string> GroupNames { get; set; }
+
+        // Информация за музикантите
+        public List<int> MusicianIds { get; set; }
+        public List<string> MusicianUsernames { get; set; }
+
+        // Информация за събитията
+        public List<int> EventIds { get; set; }
+        public List<string> EventNames { get; set; }
+        public List<DateTime> EventDates { get; set; }
+
+        // Търсене
+        public string SearchQuery { get; set; }
+    }
+}
